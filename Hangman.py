dc52007a15b91f11ee745864a93092597fba3299
Abdulazeez Ibrahim <71079098+Earl-Erbdoul@users.noreply.github.com>
2020-10-23 21:45:22 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -6,30 +6,27 @@ secretList = ["control", "remote", "laptop", "cognition", "shelf", "book", "hous
               "picture", "food", "mind", "brave", "robot", "conflict", "school", "forever", "tea", "car", "library",
               "great", "paradox", "insomnia", "yogurt", "time", "illusion", "vendetta", "crown", "world", "electrical",
               "wallpaper", "clock", "monkey", "space", "movie", "order", "chaos", "knight", "mystery", "destination"]
+# choose a random word
 secretWord = random.choice(secretList)
 guesses = ""
 noOfGuesses = 5
-
 while noOfGuesses > 0:
     wrongGuess = 0
-
+    # check for letter guessed by user
     for letter in secretWord:
         if letter in guesses:
             print(letter, end=" ")
         else:
             print("_", end=" ")
             wrongGuess += 1
-
     if wrongGuess == 0:
         print("\nCongratulations. You win.")
         break
     guess = input("\nGuess a letter: ")
     guesses += guess
-
     if guess not in secretWord:
         noOfGuesses -= 1
         print("Wrong letter guess.")
         print("You have", + noOfGuesses, "more guesses left.")
-
 if noOfGuesses == 0:
     print("You Lose. ", secretWord, " is the word")
